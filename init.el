3c95896302039ec936c2ac5c0093fe2b9ef3f65b
fsq120 <fsq120@gmail.com>
2014-09-03 10:59:40 +0800

    1.0 release
    add window keep

--- init.el
+++ init.el
@@ -197,3 +197,35 @@ try-complete-lisp-symbol))
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "black" :foreground "#00ff00" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 128 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
+
+ ;;
+;; win:resume-windows --> windows 状态保存跟复原
+;;______________________________________________________________________
+ 
+;;; 安装
+;; http://www.emacswiki.org/emacs/WindowsMode
+;; http://www.gentei.org/~yuuji/software/
+;; (install-elisp "http://www.gentei.org/~yuuji/software/windows.el")
+;; (install-elisp "http://www.gentei.org/~yuuji/software/revive.el")
+ 
+;;; windows.el 快捷键设定
+(setq win:switch-prefix "\C-z")
+;; 布局信息文件保存位置
+(setq win:configuration-file "~/.windows")
+(require 'windows)
+ 
+(setq win:use-frame nil)
+(win:startup-with-window)
+(define-key ctl-x-map "C" 'see-you-again)
+ 
+;; 启动时自动复原
+(add-hook 'after-init-hook (lambda() (run-with-idle-timer 0 nil 'my-resume-windows)))
+(defun my-resume-windows ()
+  "restore windows status ."
+  (interactive)
+   (resume-windows t)
+   (clear-active-region-all-buffers)
+  )
+ 
+;; 关闭时自动保存
+(add-hook 'kill-emacs-hook 'win-save-all-configurations)
